a4e5de3b6eb49cfc2e6bc52031f894061529c820
Daniel Hernán Rico <danielhernanrico@gmail.com>
2020-10-20 15:13:08 -0300

    guessinggame completed

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 secret=$(ls | wc -l)
-
-echo $secret
+guess=-1
+echo -n "How many files are in this directory"
+while [ $guess -ne $secret ]
+do
+	echo -n "? "
+	read guess
+	if [ $guess -lt $secret ]
+	then
+		echo "Too low."
+	elif [ $guess -gt $secret ]
+	then
+		echo "Too High."
+	fi
+done
+echo "Well done!."
